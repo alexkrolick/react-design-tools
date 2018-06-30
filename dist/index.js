6c97b96ff79ea7891e07e13bf0564d65b6e8648c
Alex Krolick <akrolick@box.com>
2018-06-29 22:53:42 -0700

    Add CJS build

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.PaletteLookup = exports.DiffTool = undefined;
+
+var _diffTool = require("./diff-tool");
+
+var _diffTool2 = _interopRequireDefault(_diffTool);
+
+var _paletteLookup = require("./palette-lookup");
+
+var _paletteLookup2 = _interopRequireDefault(_paletteLookup);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//  strict
+exports.DiffTool = _diffTool2.default;
+exports.PaletteLookup = _paletteLookup2.default;
